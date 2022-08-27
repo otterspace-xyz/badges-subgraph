276a68dc5b73c4bc86910988f16d96e55a1dfe93
Rahul Rumalla <rahulrumalla@gmail.com>
2022-08-27 12:24:30 +0200

    Updated spec uri format

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,13 +1,26 @@
 import { Badges, SpecCreated, Transfer } from '../generated/Badges/Badges';
 import { BadgeSpec, Badge } from '../generated/schema';
+import { ethereum, BigInt } from '@graphprotocol/graph-ts';
+
+const metadataPart = '/metadata.json';
+
+function getCID(uri: string): string {
+  return uri.substring(uri.lastIndexOf('/') + 1);
+}
+
+function getMetadataUri(uri: string): string {
+  return uri.concat(metadataPart);
+}
 
 export function handleSpecCreated(event: SpecCreated): void {
-  const id = event.params.specUri;
+  const id = getCID(event.params.specUri);
+  const uri = getMetadataUri(event.params.specUri);
   const raftTokenId = event.params.raftTokenId;
   const raftAddress = event.params.raftAddress;
   const timestamp = event.block.timestamp;
 
   let spec = new BadgeSpec(id);
+  spec.uri = uri;
   spec.raftTokenId = raftTokenId;
   spec.raftTokenAddress = raftAddress;
   spec.createdAtTimestamp = timestamp;
@@ -23,17 +36,18 @@ export function handleBadgeMinted(event: Transfer): void {
 
   const badgesContract = Badges.bind(event.address);
   const specUri = badgesContract.tokenURI(id);
+  const specId = getCID(specUri);
 
   let badge = new Badge(id.toString());
   badge.from = from;
   badge.owner = to;
-  badge.spec = specUri;
+  badge.spec = specId;
   badge.createdAtTimestamp = timestamp;
   badge.save();
 
-  const spec = BadgeSpec.load(specUri);
+  const spec = BadgeSpec.load(specId);
   if (spec !== null) {
-    spec.totalBadgesCount++;
+    spec.totalBadgesCount += 1;
     spec.save();
   }
 }
