5a60e76f0575a462332774a43a20ea0a75009c98
Rahul Rumalla <rahulrumalla@gmail.com>
2022-08-31 13:58:35 +0200

    Updated urns for entities. Added url for raft

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,29 +1,41 @@
-import { Badges, SpecCreated, Transfer as BadgeTransfer } from '../generated/Badges/Badges';
-import { Transfer as RaftTransfer } from '../generated/Raft/Raft';
+import {
+  Badges as BadgesContract,
+  SpecCreated,
+  Transfer as BadgeTransfer,
+} from '../generated/Badges/Badges';
+import { Transfer as RaftTransfer, Raft as RaftContract } from '../generated/Raft/Raft';
 import { BadgeSpec, Badge, Raft } from '../generated/schema';
 import { BigInt, Address, log } from '@graphprotocol/graph-ts';
 
 const metadataPart = '/metadata.json';
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+const raftsUrnNamespace = 'rafts';
+const badgesUrnNamespace = 'badges';
 
+// returns a string representing the raftID in the format `rafts:raftAddress:raftTokenId`
 function getRaftID(raftTokenId: BigInt, raftAddress: Address): string {
-  return raftAddress
-    .toHexString()
+  return raftsUrnNamespace
+    .concat(':')
+    .concat(raftAddress.toHexString())
     .concat(':')
     .concat(raftTokenId.toString());
 }
 
+// returns a string that is a CID extracted from the IPFS uri
 function getSpecID(uri: string): string {
   return uri.substring(uri.lastIndexOf('/') + 1);
 }
 
+// returns a string representing a unique badgeID in the format `badges:badgeAddress:badgeTokenId`
 function getBadgeID(badgeTokenId: BigInt, badgeAddress: Address): string {
-  return badgeAddress
-  .toHexString()
-  .concat(':')
-  .concat(badgeTokenId.toString());
+  return badgesUrnNamespace
+    .concat(':')
+    .concat(badgeAddress.toHexString())
+    .concat(':')
+    .concat(badgeTokenId.toString());
 }
 
+// returns a fully formed metadata uri for raft & badge metadata
 function getMetadataUri(uri: string): string {
   return uri.concat(metadataPart);
 }
@@ -36,6 +48,7 @@ export function handleRaftTransfer(event: RaftTransfer): void {
 
   let raft = Raft.load(raftID);
   if (raft !== null) {
+    // Raft was transferred to a new owner
     raft.owner = to;
   } else {
     raft = new Raft(raftID);
@@ -44,6 +57,9 @@ export function handleRaftTransfer(event: RaftTransfer): void {
     raft.totalBadgesCount = 0;
     raft.totalSpecsCount = 0;
     raft.createdAtTimestamp = timestamp;
+
+    const raftContract = RaftContract.bind(event.address);
+    raft.uri = raftContract.tokenURI(tokenId);
   }
   raft.save();
 }
@@ -68,7 +84,7 @@ export function handleSpecCreated(event: SpecCreated): void {
     raft.totalSpecsCount += 1;
     raft.save();
   } else {
-    log.critical('Raft not found: {raftID}', [raftID]);
+    log.error('handleSpecCreated: Raft {} not found. Raft entity was not updated', [raftID]);
   }
 }
 
@@ -76,7 +92,7 @@ export function handleBadgeTransfer(event: BadgeTransfer): void {
   const from = event.params.from.toHexString();
   const tokenId = event.params.tokenId;
   const badgeAddress = event.address;
-  const badgeId = getBadgeID(tokenId, badgeAddress)
+  const badgeId = getBadgeID(tokenId, badgeAddress);
   if (from == ZERO_ADDRESS) {
     handleBadgeMinted(badgeId, event);
   } else {
@@ -90,7 +106,7 @@ function handleBadgeMinted(badgeId: string, event: BadgeTransfer): void {
   const to = event.params.to;
   const timestamp = event.block.timestamp;
 
-  const badgesContract = Badges.bind(event.address);
+  const badgesContract = BadgesContract.bind(event.address);
   const specUri = badgesContract.tokenURI(tokenId);
   const specID = getSpecID(specUri);
 
@@ -112,10 +128,13 @@ function handleBadgeMinted(badgeId: string, event: BadgeTransfer): void {
       raft.totalBadgesCount += 1;
       raft.save();
     } else {
-      log.critical('Raft not found: {raftID}', [raftID]);
+      log.error(
+        'handleBadgeMinted: RaftID {} not found. Raft entity was not updated with totalBadgesCount',
+        [raftID],
+      );
     }
   } else {
-    log.critical('Spec not found: {specID}', [specID]);
+    log.error('handleBadgeMinted: Spec {} not found. Badge entity was not created', [specID]);
   }
 }
 
@@ -126,5 +145,10 @@ function handleBadgeBurned(badgeId: string, event: BadgeTransfer): void {
   if (badge !== null) {
     badge.burnedAtTimestamp = timestamp;
     badge.save();
+  } else {
+    log.error(
+      'handleBadgeBurned: Badge {} not found. Badge entity was not updated with burnedAtTimestamp',
+      [badgeId],
+    );
   }
 }
