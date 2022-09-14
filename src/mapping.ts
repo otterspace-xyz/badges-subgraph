5fdc79af2cea65a29d86de3cf0e9ef16aaeaabce
dsh-otter <dasha@otterspace.xyz>
2022-09-14 11:15:19 +0200

    Revert "[DEV] mapping refactoring"
    This reverts commit edd57d0f207efd354a7138037d91399a0ec43395.

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,16 +1,37 @@
-import { SpecCreated, Transfer as BadgeTransfer } from '../generated/Badges/Badges';
+import { Badges, SpecCreated, Transfer as BadgeTransfer } from '../generated/Badges/Badges';
 import { Transfer as RaftTransfer } from '../generated/Raft/Raft';
-import { BadgeSpec, Raft } from '../generated/schema';
-import { log } from '@graphprotocol/graph-ts';
-import { getTokenID, getSpecID, getMetadataUri } from './utils/helper';
-import { handleBadgeMinted, handleBadgeBurned } from './badges';
+import { BadgeSpec, Badge, Raft } from '../generated/schema';
+import { BigInt, Address, log } from '@graphprotocol/graph-ts';
 
+const metadataPart = '/metadata.json';
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+function getRaftID(raftTokenId: BigInt, raftAddress: Address): string {
+  return raftAddress
+    .toHexString()
+    .concat(':')
+    .concat(raftTokenId.toString());
+}
+
+function getSpecID(uri: string): string {
+  return uri.substring(uri.lastIndexOf('/') + 1);
+}
+
+function getBadgeID(badgeTokenId: BigInt, badgeAddress: Address): string {
+  return badgeAddress
+  .toHexString()
+  .concat(':')
+  .concat(badgeTokenId.toString());
+}
+
+function getMetadataUri(uri: string): string {
+  return uri.concat(metadataPart);
+}
 
 export function handleRaftTransfer(event: RaftTransfer): void {
   const to = event.params.to;
   const tokenId = event.params.tokenId;
-  const raftID = getTokenID(tokenId, event.address);
+  const raftID = getRaftID(tokenId, event.address);
   const timestamp = event.block.timestamp;
 
   let raft = Raft.load(raftID);
@@ -32,7 +53,7 @@ export function handleSpecCreated(event: SpecCreated): void {
   const uri = getMetadataUri(event.params.specUri);
   const raftAddress = event.params.raftAddress;
   const raftTokenId = event.params.raftTokenId;
-  const raftID = getTokenID(raftTokenId, raftAddress);
+  const raftID = getRaftID(raftTokenId, raftAddress);
   const timestamp = event.block.timestamp;
 
   let spec = new BadgeSpec(id);
@@ -55,10 +76,55 @@ export function handleBadgeTransfer(event: BadgeTransfer): void {
   const from = event.params.from.toHexString();
   const tokenId = event.params.tokenId;
   const badgeAddress = event.address;
-  const badgeId = getTokenID(tokenId, badgeAddress)
+  const badgeId = getBadgeID(tokenId, badgeAddress)
   if (from == ZERO_ADDRESS) {
     handleBadgeMinted(badgeId, event);
   } else {
     handleBadgeBurned(badgeId, event);
+  }
+}
+
+function handleBadgeMinted(badgeId: string, event: BadgeTransfer): void {
+  const tokenId = event.params.tokenId;
+  const from = event.params.from;
+  const to = event.params.to;
+  const timestamp = event.block.timestamp;
+
+  const badgesContract = Badges.bind(event.address);
+  const specUri = badgesContract.tokenURI(tokenId);
+  const specID = getSpecID(specUri);
+
+  let badge = new Badge(badgeId);
+  badge.from = from;
+  badge.owner = to;
+  badge.spec = specID;
+  badge.createdAtTimestamp = timestamp;
+  badge.save();
+
+  const spec = BadgeSpec.load(specID);
+  if (spec !== null) {
+    spec.totalBadgesCount += 1;
+    spec.save();
+
+    const raftID = spec.raft;
+    const raft = Raft.load(raftID);
+    if (raft !== null) {
+      raft.totalBadgesCount += 1;
+      raft.save();
+    } else {
+      log.critical('Raft not found: {raftID}', [raftID]);
+    }
+  } else {
+    log.critical('Spec not found: {specID}', [specID]);
+  }
+}
+
+function handleBadgeBurned(badgeId: string, event: BadgeTransfer): void {
+  const timestamp = event.block.timestamp;
+
+  const badge = Badge.load(badgeId);
+  if (badge !== null) {
+    badge.burnedAtTimestamp = timestamp;
+    badge.save();
   }
 }
