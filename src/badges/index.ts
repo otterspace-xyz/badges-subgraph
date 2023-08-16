a07853f51beea652329c3fd8aa02b43d4ae34837
Rahul Rumalla <rahulrumalla@gmail.com>
2023-08-16 23:45:08 +0200

    feat: adds counters for revocations and holders

--- src/badges/index.ts
+++ src/badges/index.ts
@@ -35,27 +35,28 @@ export function handleBadgeMinted(badgeId: string, event: BadgeTransfer): void {
     log.error('handleBadgeMinted: SpecID {} not found. Badge entity was not created', [specID]);
   }
 
+  // update user entity
+  let isUserNewBadgeHolder = false;
+  let user = User.load(to);
+  if (user !== null) {
+    user.totalBadgesCount += 1;
+  } else {
+    user = new User(to);
+    user.totalBadgesCount = 0;
+    isUserNewBadgeHolder = true;
+  }
+  user.save();
+
   // update raft entity
   const raft = Raft.load(raftID);
   if (raft !== null) {
     raft.totalBadgesCount += 1;
+    if (isUserNewBadgeHolder) raft.totalBadgeHoldersCount += 1;
     raft.save();
   } else {
     log.error(
       'handleBadgeMinted: RaftID {} not found. Raft entity was not updated with totalBadgesCount',
       [raftID],
-    );
-  }
-
-  // update user entity
-  const user = User.load(to);
-  if (user !== null) {
-    user.totalBadgesCount += 1;
-    user.save();
-  } else {
-    log.error(
-      'handleBadgeMinted: UserID {} not found. User entity was not updated with totalBadgesCount',
-      [to.toHexString()],
     );
   }
 }
